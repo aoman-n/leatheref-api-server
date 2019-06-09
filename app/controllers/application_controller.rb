69deceb1731a38125f43374c7b3037b694b46847
aoba <ipbdipbd00@gmail.com>
2019-06-10 04:03:09 +0900

    Isolate response module

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::API
   include ActionController::HttpAuthentication::Token::ControllerMethods
+  include ResponseModule
   include SessionsHelper
 
   # unless Rails.env.development?
@@ -19,48 +20,5 @@ class ApplicationController < ActionController::API
 
   def logged_in_user
     response_unauthorized unless logged_in?
-  end
-
-  # 200 Success
-  def response_success(class_name, action_name)
-    render status: 200, json: {
-      status: 200,
-      message: "Success #{class_name.capitalize} #{action_name.capitalize}",
-    }
-  end
-
-  # 400 Bad Request
-  def response_bad_request
-    render status: 400, json: { status: 400, message: 'Bad Request' }
-  end
-
-  # 401 Unauthorized
-  def response_unauthorized
-    render status: 401, json: { status: 401, message: 'Unauthorized' }
-  end
-
-  # 404 Not Found
-  def response_not_found(class_name: 'page')
-    render status: 404, json: { status: 404, message: "#{class_name.capitalize} Not Found" }
-  end
-
-  def custom_error_404(error: nil)
-    logger.info "Rendering 404 with exception: #{error.message}" if error
-    render status: 404, json: { status: 404, message: 'Not Found' }
-  end
-
-  # 409 Conflict
-  def response_conflict(class_name)
-    render status: 409, json: { status: 409, message: "#{class_name.capitalize} Conflict" }
-  end
-
-  # 500 Internal Server Error
-  def response_internal_server_error
-    render status: 500, json: { status: 500, message: 'Internal Server Error' }
-  end
-
-  def custom_error_500(error: nil)
-    logger.error "Rendering 500 with exception: #{error.message}" if error
-    render status: 500, json: { status: 500, message: "Internal Server Error #{error}" }
   end
 end

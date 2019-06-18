722a327343ead008b0562801fb4680886429da84
aoba <ipbdipbd00@gmail.com>
2019-06-19 01:16:20 +0900

    Implement picture uploader of review

--- app/models/review.rb
+++ app/models/review.rb
@@ -21,10 +21,22 @@ class Review < ApplicationRecord
   belongs_to :store
   belongs_to :product_category
 
+  mount_uploader :picture, PictureUploader
+
   validates :product_name, presence: true, length: { minimum: 4 }
   validates :content, presence: true, length: { maximum: 500 }
   validates :rating, presence: true
   validates_inclusion_of :rating, in: 1..10
+  validate :picture_size
 
   scope :recent, -> { order(created_at: :desc) }
+
+  private
+
+  # アップロードされた画像のサイズをバリデーションする
+  def picture_size
+    if picture.size > 5.megabytes
+      errors.add(:picture, "should be less than 5MB")
+    end
+  end
 end

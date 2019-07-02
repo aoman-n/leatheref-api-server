71acd3ddbcd0260ba7f53997a12874b0f495fabc
aoba <ipbdipbd00@gmail.com>
2019-07-02 22:47:47 +0900

    Add user, revew to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,26 @@
-CONFIG[:stores].each { |n| Store.create!({ name: n }) }
+ActiveRecord::Base.transaction do
+  CONFIG[:stores].each { |n| Store.create!({ name: n }) }
+  CONFIG[:product_categories].each { |n| ProductCategory.create!({ name: n }) }
 
-CONFIG[:product_categories].each { |n| ProductCategory.create!({ name: n }) }
+  if Rails.env.development?
+    stores = Store.all
+    product_categories = ProductCategory.all
+    5.times do |i|
+      user = User.create!(
+        login_name: "user#{i}",
+        email: "user#{i}@example.com",
+        password: 'password',
+        activated: true,
+        image_url: 'https://s3-ap-northeast-1.amazonaws.com/aohiro-blog/User/avatar/dot.jp',
+      )
+      user.reviews.create!(
+        product_name: "商品名#{i}",
+        content: "テストレビュー#{i}",
+        price: 100,
+        rating: i + 1,
+        store_id: stores.sample.id,
+        product_category_id: product_categories.sample.id,
+      )
+    end
+  end
+end
